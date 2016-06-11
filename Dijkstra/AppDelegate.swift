aea49335108c914d7e90d7146d61d30c4dddf493
Joost van Breukelen <Joost@imac-2.fritz.box>
2016-06-11 14:20:33 +0200

    Initial Commit

---
+++ Dijkstra/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dijkstra
+//
+//  Created by Joost van Breukelen on 11-06-16.
+//  Copyright © 2016 Joost van Breukelen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dijkstra/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dijkstra
+//
+//  Created by Joost van Breukelen on 11-06-16.
+//  Copyright © 2016 Joost van Breukelen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
